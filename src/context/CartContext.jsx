20fcbfce5dec8ff48d7cf5b90a637615fc6f9bfb
Fabrício Silva <fabricio85silva@hotmail.com>
2024-04-13 13:56:52 -0300

    add itens no carrinho com localstorage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,16 +3,27 @@ import { createContext, useEffect, useState } from "react";
 export const CartContext = createContext()
 
 export const CartProvider = ({children})=>{
+
+
+
     const [cartItems, setCartItems] = useState([])
+   
+
+    useEffect(()=>{
+            
+       
+
+    },[cartItems])
 
     
     const addCartContext = (item)=>{
-        setCartItems(item)
-          
+         setCartItems(item)
+         localStorage.setItem('prodCart', JSON.stringify(item))
     }
+        
+    
 
-
-    return <CartContext.Provider value={{cartItems, addCartContext}}>
+    return <CartContext.Provider value={{cartItems, addCartContext, setCartItems}}>
         {children}
     </CartContext.Provider>
 }
